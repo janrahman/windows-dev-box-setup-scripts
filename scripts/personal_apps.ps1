1ae7ab840f52ca90994e54a9d3ea0d2019e3742d
Jan Rahman <44271158+janrahman@users.noreply.github.com>
2020-10-25 16:07:40 +0100

    create personal_apps

---
+++ scripts/personal_apps.ps1
@@ -0,0 +1,9 @@
+choco install -y vscode
+choco install -y notepadplusplus.install
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration'"
+choco install -y 7zip.install
+choco install -y firefox --package-parameters="'/NoAutoUpdate'"
+choco install -y thunderbird --package-parameters="'/NoAutoUpdate'"
+choco install -y vlc
+choco install -y foobar2000 --package-parameters="'/NoShortcut'"
+choco install -y irfanview 
